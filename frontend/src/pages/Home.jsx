d4b470980063316a7a3b37fb65c7a7f194168b36
Maxime Dupin <maximedupin@live.fr>
2023-01-29 23:30:01 +0100

    Essai axios sur front

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
 
 function Home() {
+  const [bossList, setBossList] = useState([]);
+
+  const getBossList = () => {
+    const url = "http://localhost:8000/api/boss";
+    axios.get(url).then((response) => setBossList(response.data));
+  };
+
+  useEffect(() => {
+    getBossList();
+  }, []);
+
   return (
     <div>
       <p>Home</p>
+      <ul>
+        {bossList.map((element) => (
+          <li>{element.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }

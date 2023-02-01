a7234d6ab86ec57ec74498a0d2419c09f46fa366
Maxime Dupin <maximedupin@live.fr>
2023-02-01 15:47:20 +0100

    WIP front

--- frontend/src/pages/BossDetails.jsx
+++ frontend/src/pages/BossDetails.jsx
@@ -19,22 +19,37 @@ function BossDetails() {
   }, []);
 
   return (
-    <div>
-      {oneBoss.map((element) => (
-        <div className="card">
-          <span className="name">{element.name}</span>
-          <span className="location">{element.location}</span>
-          <span className="mandatory">{`Obligatoire: ${element.mandatory}`}</span>
-          <div className="description">{element.description}</div>
-          <span className="difficulty">{`🌶️ ${element.difficulty}/10`}</span>
-          <div
-            style={{ backgroundImage: `url(${element.picture}` }}
-            className="image"
-          >
-            {/* <ImageCard picture={picture} /> */}
-          </div>
-        </div>
-      ))}
+    <div className="general">
+      <div className="div_card">
+        {oneBoss.map((element) => (
+          <>
+            <div className="card_details">
+              <span className="name_details">{element.name}</span>
+              <span className="location_details">{element.location}</span>
+              <span className="mandatory_details">{`Obligatoire: ${element.mandatory}`}</span>
+              <div className="description_details">{element.description}</div>
+              <span className="difficulty_details">{`🌶️ ${element.difficulty}/10`}</span>
+              <div
+                style={{ backgroundImage: `url(${element.picture}` }}
+                className="image_details"
+              >
+                {/* <ImageCard picture={picture} /> */}
+              </div>
+            </div>
+            <div className="video_details">
+              <div className="div_combat">
+                <p className="combat">DÉROULEMENT DU COMBAT</p>
+              </div>
+              <iframe
+                width="560"
+                height="315"
+                src={element.video}
+                title="Video"
+              />
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
